dbcf1855198af1a7582c58ed4fd26e9c00ad705c
codinci <dkibau@gmail.com>
2023-05-26 13:58:54 +0300

    added comments

--- nse_scraper.py
+++ nse_scraper.py
@@ -8,15 +8,16 @@ req=requests.get(url)
 content=req.text
 soup=BeautifulSoup(content, 'lxml')
 
+#scraps data contained within the class of t
 required_data = soup.find(class_='t').find_all('tbody')
 finalised_data = []
-# print(required_data)
+# loop through scraped data
 for data in required_data:
 	row = data.find_all('td')
 	for i in row:
-		formatted=i.get_text(strip=True)
-		finalised_data.append(formatted)
-	
+		formatted=i.get_text(strip=True)#strips it of html tags
+		finalised_data.append(formatted)#adds the formatted data to the finalised_data array
+
 
 print(finalised_data)
 
